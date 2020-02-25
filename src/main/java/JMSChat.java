e4d1b8b90e226c0879973036d50fe4ce15ed0e59
moussa22 <cheickoumar6yahoo.fr>
2020-02-24 21:35:59 -0500

    commit add imges folder

--- src/main/java/JMSChat.java
+++ src/main/java/JMSChat.java
@@ -1,15 +1,20 @@
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import org.apache.activemq.ActiveMQConnectionFactory;
 
 import javax.jms.*;
+import java.io.File;
 
 public class JMSChat extends Application {
 
@@ -57,25 +62,40 @@ public class JMSChat extends Application {
         borderPane.setCenter(vBox);
         Label labelTo=new Label("To");
         TextField textFieldTo=new TextField("C1");
-        textFieldTo.setPrefWidth(150);
+        textFieldTo.setPrefWidth(250);
         Label labelMessage=new Label("Message");
         TextArea textAreMessage=new TextArea();
         Button buttonSend=new Button("Send");
-        textAreMessage.setPrefWidth(150);
+        textAreMessage.setPrefWidth(250);
         Label labelImage=new Label("Image");
-        ComboBox<String> comboBoxImage=new ComboBox<String>();
+        File file=new File("images");
+        ObservableList<String> observableList= FXCollections.observableArrayList(file.list());
+        ComboBox<String> comboBoxImage=new ComboBox<String>(observableList);
+        comboBoxImage.getSelectionModel().select(0);
         Button buttonSendImage=new Button("Send Image");
         gridPane.setPadding(new Insets(10));
         gridPane.setVgap(10);
         gridPane.setHgap(10);
-        textAreMessage.setPrefRowCount(2);
+        textAreMessage.setPrefRowCount(1);
         gridPane.add(labelTo,0,0); gridPane.add(textFieldTo,1,0);
         gridPane.add(labelMessage,0,1);
         gridPane.add(textAreMessage,1,1);
         gridPane.add(buttonSend,2,1);
         gridPane.add(labelImage,0,2);
         gridPane.add(comboBoxImage,1,2);
-        gridPane.add(buttonSendImage,1,2);
+        gridPane.add(buttonSendImage,2,2);
+        ObservableList<String> observableListMesage= FXCollections.observableArrayList();
+
+        ListView<String> listViewMessages=new ListView<>(observableListMesage);
+        File f2=new File("images/"+comboBoxImage.getSelectionModel().getSelectedItem());
+        Image image=new Image(f2.toURL().toString());
+        ImageView imageView=new ImageView(image);
+        imageView.setFitWidth(320);
+        imageView.setFitHeight(240);
+        hBox2.getChildren().add(listViewMessages);
+        hBox2.getChildren().add(imageView);
+        hBox2.setPadding(new Insets(10));
+        hBox2.setSpacing(10);
 
         Scene scene=new Scene(borderPane,800,500);
 
